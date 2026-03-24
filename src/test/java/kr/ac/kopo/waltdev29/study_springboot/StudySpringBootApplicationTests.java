8dc94c3fa81dbb83d25221931f2d4dcbde4fad6d
WaltDev29 <waltdev29@gmail.com>
2026-03-24 12:04:21 +0900

    initial commit

---
+++ src/test/java/kr/ac/kopo/waltdev29/study_springboot/StudySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.waltdev29.study_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
